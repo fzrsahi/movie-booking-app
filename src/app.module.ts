baeaac49e5b53037035c0f897552b4c9fb79e5f8
fzrsahi <fazrul.anugrah17@gmail.com>
2023-06-28 17:54:37 +0800

    add : feature movie

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MovieModule } from './movie/movie.module';
+import { PrismaService } from './prisma/prisma.service';
+
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { BalanceModule } from './balance/balance.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MovieModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BalanceModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
---
+++ src/movie/movie.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { MovieService } from './movie.service';
+
+@Controller('/movie')
+export class MovieController {
+  constructor(private movieService: MovieService) {}
+
+  @Get()
+  getAllMovie(
+    @Query('limit', ParseIntPipe) limit: number,
+    @Query('page', ParseIntPipe) page: number,
+  ) {
+    return this.movieService.getAllMovies(page, limit);
+  }
+
+  @Get(':id')
+  getMovieById(@Param('id', ParseIntPipe) movieId) {
+    return this.movieService.getMovieById(movieId);
+  }
+}
