2874aed397a3d2a5f32b751c8021aa8a69a865af
fzrsahi <fazrul.anugrah17@gmail.com>
2023-07-01 00:10:14 +0800

    fix dotenv

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
