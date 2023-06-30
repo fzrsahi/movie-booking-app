4b08e9e3b47069797a7cf7aa8d74a70234e447c8
fzrsahi <fazrul.anugrah17@gmail.com>
2023-07-01 00:50:42 +0800

    fix beberapa

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
+  console.log(PORT);
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
