392b92d2798179fc88b6e1b0443f6228e6c45159
fzrsahi <fazrul.anugrah17@gmail.com>
2023-07-01 01:28:24 +0800

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
-  console.log(PORT);
-
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
   await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
