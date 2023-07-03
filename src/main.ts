4fee3d92c41c379fd722875d542f8b8f50451221
fzrsahi <fazrul.anugrah17@gmail.com>
2023-07-03 23:24:54 +0800

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   app.enableCors();
-  await app.listen(PORT, '0.0.0.0');
+  await app.listen(3000);
 }
 bootstrap();
